87cca9d0ce77687dbda27e9ee750f9d0c592a3d7
Lucas Claro <lucasbrclaro@gmail.com>
2022-08-12 18:30:39 -0300

    só preciso testar

--- app/src/main/java/br/senac/tcc/MainActivity.kt
+++ app/src/main/java/br/senac/tcc/MainActivity.kt
@@ -1,28 +1,58 @@
 package br.senac.tcc
 
+import android.content.BroadcastReceiver
+import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import br.senac.tcc.databinding.ActivityMainBinding
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var b: ActivityMainBinding
 
+    lateinit var wifiManager: WifiManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
 
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
         b.btnTestar.setOnClickListener { teste() }
     }
 
+    override fun onStart() {
+        super.onStart()
+        val intentFilter = IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION)
+        registerReceiver(wifiScanReceiver, intentFilter)
+    }
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(wifiScanReceiver)
+    }
+
+    private val wifiScanReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val wifiScanList: List<ScanResult> = wifiManager.getScanResults()
+            for (wifiScan in wifiScanList) {
+                val name = wifiScan.BSSID.lowercase(Locale.getDefault())
+                val rssi = wifiScan.level
+                Log.v(TAG, "wifi: " + name + " => " + rssi + "dBm")
+            }
+        }
+    }
+
     private fun teste() {
-        val wifiManager = this.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        val info = wifiManager.is5GHzBandSupported
-        println(info)
+        wifiManager.startScan()
         //val ssid = info.ssid
         //val rssi = info.rssi
     }
