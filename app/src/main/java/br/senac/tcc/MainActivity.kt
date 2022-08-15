5b432874ed63ec35f09f9ee8a08174160ad178b6
Lucas Claro <lucasbrclaro@gmail.com>
2022-08-15 18:55:32 -0300

    Consegui pegar as redes

--- app/src/main/java/br/senac/tcc/MainActivity.kt
+++ app/src/main/java/br/senac/tcc/MainActivity.kt
@@ -25,34 +25,90 @@ class MainActivity : AppCompatActivity() {
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
 
+        Log.v(TAG, "11111111111111")
         wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        Log.v(TAG, "2222222222222")
+        //verifierPermission()
 
         b.btnTestar.setOnClickListener { teste() }
     }
 
+//    private fun verifierPermission() {
+//        Log.v(TAG, "33333333333333333333")
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CHANGE_WIFI_MULTICAST_STATE)!= PackageManager.PERMISSION_GRANTED) {
+//            /ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CHANGE_WIFI_MULTICAST_STATE), 1)
+//        } else {
+//            Log.v(TAG, "555555555555555555555")
+//            permissaoConcedida = true
+//            wifiManager.startScan()
+//        }
+//    }
+
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        Log.v(TAG, "444444444444444444444")
+//
+//        if (requestCode == 1) {
+//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                permissaoConcedida = true
+//                wifiManager.startScan()
+//            }
+//            else if (!shouldShowRequestPermissionRationale(permissions[0])) {
+//                Toast.makeText(this, "Not Allowed", Toast.LENGTH_LONG).show()
+//            }
+//            else {
+//                permissaoConcedida = false
+//                Toast.makeText(this, "Not Allowed", Toast.LENGTH_LONG).show()
+//                setResult(RESULT_CANCELED)
+//                finish()
+//            }
+//        }
+//    }
+
     override fun onStart() {
         super.onStart()
-        val intentFilter = IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
-        registerReceiver(wifiScanReceiver, intentFilter)
+        Log.v(TAG, "333333")
+//        val intentFilter = IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+//        registerReceiver(wifiScanReceiver, intentFilter)
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        applicationContext.registerReceiver(wifiScanReceiver, intentFilter)
     }
+
     override fun onStop() {
         super.onStop()
         unregisterReceiver(wifiScanReceiver)
     }
 
-    private val wifiScanReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+    val wifiScanReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
-            val wifiScanList: List<ScanResult> = wifiManager.getScanResults()
-            for (wifiScan in wifiScanList) {
-                val name = wifiScan.BSSID.lowercase(Locale.getDefault())
-                val rssi = wifiScan.level
-                Log.v(TAG, "wifi: " + name + " => " + rssi + "dBm")
+            Log.v(TAG, "444444444444444444444")
+            val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+            if (success) {
+                Log.v(TAG, "666666666")
+                val wifiScanList: List<ScanResult> = wifiManager.scanResults
+                Log.v(TAG, wifiScanList.toString())
+                for (wifiScan in wifiScanList) {
+                    val name = wifiScan.BSSID.lowercase(Locale.getDefault())
+                    val rssi = wifiScan.level
+                    Log.v(TAG, "wifi: " + name + " => " + rssi + "dBm")
+                }
+            }
+            else {
+                Log.v(TAG, "XXXXXX")
             }
         }
     }
 
     private fun teste() {
+        //verifierPermission()
+        //wifiManager.setWifiEnabled(false);
         wifiManager.startScan()
+        Log.v(TAG, "555555555")
         //val ssid = info.ssid
         //val rssi = info.rssi
     }
